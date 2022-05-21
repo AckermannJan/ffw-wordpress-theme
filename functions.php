cb82d7da5e4c5e8645269375db93215bec0242a8
Jan Ackermann <eric-ackermann@hotmail.de>
2022-05-21 17:51:07 +0200

    Add sideBarPosts

--- functions.php
+++ functions.php
@@ -1,24 +1,25 @@
 <?php
-// Remove all default WP template redirects/lookups
-remove_action( 'template_redirect', 'redirect_canonical' );
-
-// Redirect all requests to index.php so the Vue app is loaded and 404s aren't thrown
-function remove_redirects() {
-    add_rewrite_rule( '^/(.+)/?', 'index.php', 'top' );
-}
-add_action( 'init', 'remove_redirects' );
-
-// Load scripts
-function load_vue_scripts() {
-    wp_enqueue_script(
-        'vuejs-wordpress-theme-starter-js',
-        get_stylesheet_directory_uri() . '/dist/index.html',
-        array( 'jquery' ),
-        filemtime( get_stylesheet_directory() . '/dist/index.html' ),
-        true
-    );
-}
-add_action( 'wp_enqueue_scripts', 'load_vue_scripts', 100 );
+//// Remove all default WP template redirects/lookups
+//remove_action( 'template_redirect', 'redirect_canonical' );
+//
+//// Redirect all requests to index.php so the Vue app is loaded and 404s aren't thrown
+//function remove_redirects() {
+//    add_rewrite_rule( '^/(.+)/?', 'index.php', 'top' );
+//}
+//add_action( 'init', 'remove_redirects' );
+//
+//// Load scripts
+//function load_vue_scripts() {
+//    wp_enqueue_script(
+//        'vuejs-wordpress-theme-starter-js',
+//        get_stylesheet_directory_uri() . '/dist/index.html',
+//        array( 'jquery' ),
+//        filemtime( get_stylesheet_directory() . '/dist/index.html' ),
+//        true
+//    );
+//}
+//add_action( 'wp_enqueue_scripts', 'load_vue_scripts', 100 );
+//
 
 /*
  * Custom API DOC
@@ -144,11 +145,12 @@ function getSidebarInfo() {
             'sideimg' => $sideimg,
             'sideimgalt' => $sideimgalt,
         );
-        $data = (object) array_merge((array) ($post), (array) $zeitpunkt);
+        $data = (object) array_merge((array) ($post), (array) $data);
         $sideBar[] = $data;
     }
+    $sideBar = (object) array('sideBarPosts' => $sideBar);
 
-    $returnData = (object) array_merge((array) ($sideBar), (array) $nextThreeMeetings);
+    $returnData = (object) array_merge((array) $sideBar, (array) $nextThreeMeetings);
     $returnData = (object) array_merge((array) $returnData, (array) $latestAlarm);
 
     return $returnData;
